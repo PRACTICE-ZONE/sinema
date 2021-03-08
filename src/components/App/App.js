b78c707dd4a393b1c3011151dd7d51f4c8c28faf
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-09 00:05:05 +0300

    creating component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,13 @@
 import React from 'react'
 import Header from '../elements/Header/Header'
 import Home from '../Home/Home'
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
