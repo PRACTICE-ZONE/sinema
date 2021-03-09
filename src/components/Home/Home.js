14715e6205e8e0c43b37833992c99225dfb12543
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-09 19:20:57 +0300

    adding spinner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,10 @@
-import React, { Component } from 'react'
-import './Home.css'
+import React, { Component } from 'react';
+import './Home.css';
+import HeroImage from '../elements/HeroImage/HeroImage';
+import SearchBar from '../elements/SearchBar/SearchBar';
+import FourColGrid from '../elements/FourColGrid/FourColGrid';
+import MovieThumb from '../elements/MovieThumb/MovieThumb';
+import Spinner from '../elements/Spinner/Spinner';
 export default class Home extends Component {
   state = {
 
