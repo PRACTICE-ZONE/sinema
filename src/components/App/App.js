01daab090e292a3cff917471ff6b15289aeebf15
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-09 18:46:45 +0300

    creating header class

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Header from '../elements/Header/Header'
-import Home from '../Home/Home'
+import React from 'react';
+import Header from '../elements/Header/Header';
+import Home from '../Home/Home';
+import HeroImage from '../elements/HeroImage/HeroImage';
 
 const App = () => {
   return (
     <div>
       <Header/>
       <Home/>
+      <HeroImage/>
     </div>
   )
 }
