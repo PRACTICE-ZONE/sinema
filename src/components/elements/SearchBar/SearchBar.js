e5d388596a9c8c3342ec46527a65e76d16e5ba90
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-09 18:52:08 +0300

    adding search bar

---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+import './SearchBar.css'
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
