968459e411d47446ef8195d9b05a2276e789cdcb
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-09 18:25:26 +0300

    creating header class

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import './Home.css'
 export default class Home extends Component {
+  state = {
+    
+  }
   render() {
     return (
       <div>
--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -4,7 +4,10 @@ const Header = () => {
   return (
     <div>
       <div className="rmdb-header">
-        
+        <div className="rmdb-header-content">
+          <img src="./images/reactMovie_logo.png" alt="rmdb-logo" className="rmdb-logo"/>
+          <img src="./images/rmdb-logo" alt="tmdb-logo" className="rmdb-tmdb-logo"/>
+        </div>
       </div>
     </div>
   )
