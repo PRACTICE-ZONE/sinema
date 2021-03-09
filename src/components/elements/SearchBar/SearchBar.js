13aceed68e582e16f94a9f1ae2eb972ac114b666
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-09 19:05:07 +0300

    four col grid

--- src/components/elements/SearchBar/SearchBar.js
+++ src/components/elements/SearchBar/SearchBar.js
@@ -3,10 +3,13 @@ import FontAwesome from 'react-fontawesome';
 import './SearchBar.css'
 
 export default class SearchBar extends Component {
+  state = {
+
+  }
   render() {
     return (
       <div>
-        
+        SearchBar
       </div>
     )
   }
