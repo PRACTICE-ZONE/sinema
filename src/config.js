b6a7864b66d2ae2f499bf8da733fe912d07abec7
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-10 18:35:10 +0300

    adding api key

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+const API_KEY = 'b06548b53c5cf918d60696a9c800fac5';
 
 // Images
 // An image URL looks like this example:
