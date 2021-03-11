7063f633807690b7979b8738c5f4ea2907d887ff
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-11 16:41:31 +0300

    styling the hero image

--- src/components/elements/HeroImage/HeroImage.js
+++ src/components/elements/HeroImage/HeroImage.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import './HeroImage.css';
-const HeroImage = () => {
+const HeroImage = (props) => {
   return (
-    <div>
+    <div className="rmdb-heroimage"
+    style = {{
+        background: 
+        `linear-gradient(to bottom, rgba(0,0,0,0)
+        39%, rgba(0,0,0,0)
+        41%, rgba(0,0,0,0.65)
+        100%),
+        url(`${props.image}`), #1c1c1c`
+      }}
+    >
       HeroImage
     </div>
   )
