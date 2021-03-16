6d076a147e8be7e5dea18e7df38a9f81202730cd
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-16 23:28:14 +0300

    fixing bugs

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -89,7 +89,6 @@ export default class Home extends Component {
                   movieId = {element.id}
                   movieName = { element.original_title }
                   />
-                  
                 )
               })}
           </FourColGrid>
