e66c0f78ed1b1b56603c0898cb3d633074f7a94b
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-29 23:03:42 +0300

    troubleshooting

--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -4,7 +4,7 @@ const MovieThumb = (props) => {
   return (
     <div className="rmdb-moviethumb">
       { console.log("thumbnail", props.image)}
-      <img src= `"${props.image}""` alt="moviethumb"/>
+      <img src= {props.image} alt="moviethumb"/>
      </div>
   )
 }
